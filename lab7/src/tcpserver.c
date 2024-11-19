62bb2502434eac14bb583a1e7ff9e0c217a21042
KonstantinIvan <162621748+KonstantinIvan@users.noreply.github.com>
2024-11-19 22:10:59 +0000

    лр7 сделана!

--- lab7/src/tcpserver.c
+++ lab7/src/tcpserver.c
@@ -2,61 +2,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-#define SERV_PORT 10050
-#define BUFSIZE 100
 #define SADDR struct sockaddr
 
-int main() {
-  const size_t kSize = sizeof(struct sockaddr_in);
-
-  int lfd, cfd;
-  int nread;
-  char buf[BUFSIZE];
-  struct sockaddr_in servaddr;
-  struct sockaddr_in cliaddr;
-
-  if ((lfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-    perror("socket");
-    exit(1);
-  }
-
-  memset(&servaddr, 0, kSize);
-  servaddr.sin_family = AF_INET;
-  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  servaddr.sin_port = htons(SERV_PORT);
-
-  if (bind(lfd, (SADDR *)&servaddr, kSize) < 0) {
-    perror("bind");
-    exit(1);
-  }
-
-  if (listen(lfd, 5) < 0) {
-    perror("listen");
-    exit(1);
-  }
-
-  while (1) {
-    unsigned int clilen = kSize;
-
-    if ((cfd = accept(lfd, (SADDR *)&cliaddr, &clilen)) < 0) {
-      perror("accept");
-      exit(1);
-    }
-    printf("connection established\n");
-
-    while ((nread = read(cfd, buf, BUFSIZE)) > 0) {
-      write(1, &buf, nread);
+int main(int argc, char *argv[]) {
+    // Проверка на наличие достаточного количества аргументов
+    if (argc < 3) {
+        printf("Usage: %s <SERV_PORT> <BUFSIZE>\n", argv[0]);
+        exit(1);
     }
 
-    if (nread == -1) {
-      perror("read");
-      exit(1);
+    // Получение SERV_PORT и BUFSIZE из аргументов командной строки
+    int SERV_PORT = atoi(argv[1]);
+    int BUFSIZE = atoi(argv[2]);
+
+    if (SERV_PORT <= 0 || BUFSIZE <= 0) {
+        printf("Invalid port or buffer size\n");
+        exit(1);
     }
-    close(cfd);
-  }
+
+    const size_t kSize = sizeof(struct sockaddr_in);
+    int lfd, cfd;
+    int nread;
+    char buf[BUFSIZE];
+    struct sockaddr_in servaddr;
+    struct sockaddr_in cliaddr;
+
+    if ((lfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket");
+        exit(1);
+    }
+
+    memset(&servaddr, 0, kSize);
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(SERV_PORT);
+
+    if (bind(lfd, (SADDR *)&servaddr, kSize) < 0) {
+        perror("bind");
+        exit(1);
+    }
+
+    if (listen(lfd, 5) < 0) {
+        perror("listen");
+        exit(1);
+    }
+
+    while (1) {
+        unsigned int clilen = kSize;
+
+        if ((cfd = accept(lfd, (SADDR *)&cliaddr, &clilen)) < 0) {
+            perror("accept");
+            exit(1);
+        }
+        printf("Connection established\n");
+
+        while ((nread = read(cfd, buf, BUFSIZE)) > 0) {
+            write(1, buf, nread);
+        }
+
+        if (nread == -1) {
+            perror("read");
+            exit(1);
+        }
+        close(cfd);
+    }
 }
